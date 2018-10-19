edfe146aa3a9274945608f2d8180c353ff9ddf41
Ivanna Stetsyuk <istetsyuk@aplana.com>
2018-10-19 15:46:25 +0300

    Алгоритм Дейкстры

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,9 +1,47 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by Иванка on 18.10.2018.
  */
 public class Graph {
+
+    public static HashMap<String, HashMap<String, Integer>> prepareGraph() {
+        HashMap<String, HashMap<String, Integer>> graph = new HashMap<>();
+
+        HashMap<String, Integer> items = new HashMap<>();
+        items.put("A", 6);
+        items.put("B", 2);
+        graph.put("start", items);
+        items.clear();
+        items.put("finish", 1);
+        graph.put("A", items);
+        items.clear();
+        items.put("A", 3);
+        items.put("finish", 5);
+        graph.put("B", items);
+        items.clear();
+        graph.put("finish", items);
+
+        return graph;
+    }
+
+    public static HashMap<String, Integer> prepareCosts() {
+        HashMap<String, Integer> costs = new HashMap<>();
+        costs.put("A", 6);
+        costs.put("B", 2);
+        costs.put("finish", Integer.MAX_VALUE);
+        return costs;
+    }
+
+    public static HashMap<String, String> prepareParents() {
+        HashMap<String, String> parents = new HashMap<>();
+        parents.put("A", "start");
+        parents.put("B", "start");
+        parents.put("finish", null);
+        return parents;
+    }
+
 
 //    public prepareGraph() {
 //        HashMap<String, HashMap<String, Integer>> graph = new HashMap<String, HashMap<String, Integer>>();
@@ -42,16 +80,16 @@ public class Graph {
 //        System.out.println(findLowestCost(costs));
 //    }
 //
-//    private static String findLowestCost(HashMap<String, Integer> costs) {
-//        int lowestCost = Integer.MAX_VALUE;
-//        String lowestCostNode = null;
-//        for (HashMap.Entry<String, Integer> it : costs.entrySet()) {
-//            if ((it.getValue() < lowestCost) && (!processed.contains(it.getKey()))) {
-//                lowestCost = it.getValue();
-//                lowestCostNode = it.getKey();
-//            }
-//        }
-//        processed.add(lowestCostNode);
-//        return lowestCostNode;
-//    }
+    static String findLowestCost(HashMap<String, Integer> costs, ArrayList<String> processed) {
+        int lowestCost = Integer.MAX_VALUE;
+        String lowestCostNode = null;
+        for (HashMap.Entry<String, Integer> it : costs.entrySet()) {
+            if ((it.getValue() < lowestCost) && (!processed.contains(it.getKey()))) {
+                lowestCost = it.getValue();
+                lowestCostNode = it.getKey();
+            }
+        }
+        processed.add(lowestCostNode);
+        return lowestCostNode;
+    }
 }
--- src/main/java/MethodTest.java
+++ src/main/java/MethodTest.java
@@ -3,7 +3,81 @@ import java.util.*;
 
 public class MethodTest {
     public static void main(String[] args) {
-        testSumListElements();
+        testGraph();
+    }
+
+    private static void testGraph() {
+
+        ArrayList<String> processed = new ArrayList<>();
+
+        //prepare data
+        HashMap<String, HashMap<String, Integer>> graph = new HashMap<>();
+        HashMap<String, Integer> items = new HashMap<>();
+        items.put("A", 6);
+        items.put("B", 2);
+        graph.put("start", items);
+        items = new HashMap<>();
+        items.put("finish", 1);
+        graph.put("A", items);
+        items = new HashMap<>();
+        items.put("A", 3);
+        items.put("finish", 5);
+        graph.put("B", items);
+        items = new HashMap<>();
+        graph.put("finish", items);
+
+        HashMap<String, Integer> costs = new HashMap<>();
+        costs.put("A", 6);
+        costs.put("B", 2);
+        costs.put("finish", Integer.MAX_VALUE);
+
+        HashMap<String, String> parents = new HashMap<>();
+        parents.put("A", "start");
+        parents.put("B", "start");
+        parents.put("finish", null);
+
+        //search
+        String min = Graph.findLowestCost(costs, processed);
+        while (min != null) {
+            HashMap<String, Integer> elements = graph.get(min);
+            for (HashMap.Entry<String, Integer> element : elements.entrySet()) {
+                if (costs.get(min) + element.getValue() < costs.get(element.getKey())) {
+                    costs.put(element.getKey(), costs.get(min) + element.getValue());
+                    parents.put(element.getKey(), min);
+                }
+            }
+            processed.add(min);
+            min = Graph.findLowestCost(costs, processed);
+        }
+
+        //result
+        System.out.println();
+        System.out.println("Стоимости");
+        for (Map.Entry<String, Integer> el : costs.entrySet()) {
+            System.out.println(el.getKey() + " : " + el.getValue());
+        }
+
+        System.out.println();
+        System.out.println("Родители");
+        for (Map.Entry<String, String> el : parents.entrySet()) {
+            System.out.println(el.getKey() + " : " + el.getValue());
+        }
+
+        ArrayList<String> route = new ArrayList<>();
+        System.out.println();
+        System.out.println("Маршрут");
+        String currentNode = "finish";
+        while (!currentNode.equals("start")) {
+            route.add(currentNode);
+            currentNode = parents.get(currentNode);
+        }
+        route.add("start");
+        for (int i = route.size() - 1; i >= 0; i--) {
+            System.out.print(route.get(i));
+            if (i != 0) {
+                System.out.print(" -> ");
+            }
+        }
     }
 
     private static void testRecursionBinarySearch() {
